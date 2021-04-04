875de0544faa2c52c3c5d2026c86eafb9812cdfb
siwadiya <37958055+fncheng@users.noreply.github.com>
2021-04-04 15:18:37 +0800

    Create modoverrides.lua
    洞穴mod设置

---
+++ DontStarveTogether/MyDediServer/Caves/modoverrides.lua
@@ -0,0 +1,49 @@
+return {
+  -- 防卡
+  ["workshop-1216718131"]={ configuration_options={ clean=true, lang=true, stack=true }, enabled=true },
+  -- 冰箱
+  ["workshop-1898181913"]={ configuration_options={  }, enabled=true },
+  -- 智能小木牌
+  ["workshop-2069206134"]={
+    configuration_options={
+      BundleItems=true,
+      DragonflyChest=true,
+      EmptyContainerIgnored=false,
+      Icebox=false,
+      SaltBox=false 
+    },
+    enabled=true
+  },
+  -- 虫洞
+  ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=false },
+  -- 全球定位
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  },
+  -- 暖石
+  ["workshop-466732225"]={ configuration_options={  }, enabled=true },
+  -- showme
+  ["workshop-666155465"]={
+    configuration_options={
+      chestB=-1,
+      chestG=-1,
+      chestR=-1,
+      food_estimation=-1,
+      food_order=0,
+      food_style=0,
+      lang="chs",
+      show_food_units=-1,
+      show_uses=-1 
+    },
+    enabled=true
+  }
+}
