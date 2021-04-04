288017eef972d7f23a275f145d71bced18008406
siwadiya <37958055+fncheng@users.noreply.github.com>
2021-04-04 15:22:04 +0800

    Create leveldataoverride.lua
    主世界设置

---
+++ DontStarveTogether/MyDediServer/Master/leveldataoverride.lua
@@ -0,0 +1,117 @@
+return {
+  desc="标准《饥荒》体验。",
+  hideminimap=false,
+  id="SURVIVAL_TOGETHER",
+  location="forest",
+  max_playlist_position=999,
+  min_playlist_position=0,
+  name="默认",
+  numrandom_set_pieces=4,
+  override_level_string=false,
+  overrides={
+    alternatehunt="default",	-- 足迹 指 野生动物事件(大象,座狼)
+    angrybees="default",		-- 杀人蜂
+    antliontribute="default",	
+    autumn="default",			-- 开局(默认秋天)
+    bearger="default",			-- 狗熊
+    beefalo="default",			-- 牛
+    beefaloheat="default",		-- 发情的牛
+    bees="default",				-- 蜜蜂
+    berrybush="default",		-- 浆果丛
+    birds="default",
+    boons="default",
+    branching="default",
+    butterfly="default",	-- 蝴蝶
+    buzzard="default",		-- 秃鹰
+    cactus="default",			-- 仙人掌
+    carrot="default",			-- 胡萝卜
+    catcoon="default",		-- 浣熊
+    chess="often",		-- 齿轮马 默认/较多/很多
+    day="default",
+    deciduousmonster="default",
+    deerclops="default",	-- 鹿角
+    disease_delay="none", -- 疾病 default/none
+    dragonfly="default",  -- 龙蝇
+    flint="default",			-- 燧石
+    flowers="default",
+    frograin="default",   -- 青蛙雨
+    goosemoose="default",	-- 麋鹿鹅
+    grass="default",
+    has_ocean=true,
+    houndmound="default",	-- 猎犬
+    hounds="default",
+    hunt="default",
+    keep_disconnected_tiles=true,
+    krampus="default",
+    layout_mode="LinkNodesByKeys",
+    liefs="default",
+    lightning="default",
+    lightninggoat="default",
+    loop="default",
+    lureplants="default",
+    marshbush="default",
+    merm="default",
+    meteorshowers="default",
+    meteorspawner="default",
+    moles="default",	-- 鼹鼠
+    mushroom="default",
+    no_joining_islands=true,
+    no_wormholes_to_disconnected_tiles=true,
+    penguins="default",
+    perd="default",
+    petrification="default",
+    pigs="default",
+    ponds="default",
+    prefabswaps_start="default",
+    rabbits="default",
+    reeds="default",
+    regrowth="default",
+    roads="default",
+    rock="default",
+    rock_ice="default",
+    sapling="default",
+    season_start="default",
+    specialevent="default",
+    spiders="default",
+    spring="default",
+    start_location="default",
+    summer="default",
+    tallbirds="default",
+    task_set="default",
+    tentacles="default",
+    touchstone="default",
+    trees="default",
+    tumbleweed="default",
+    walrus="default",
+    weather="default",
+    wildfires="default",
+    winter="default",
+    world_size="default",
+    wormhole_prefab="wormhole" 
+  },
+  random_set_pieces={
+    "Sculptures_2",
+    "Sculptures_3",
+    "Sculptures_4",
+    "Sculptures_5",
+    "Chessy_1",
+    "Chessy_2",
+    "Chessy_3",
+    "Chessy_4",
+    "Chessy_5",
+    "Chessy_6",
+    "Maxwell1",
+    "Maxwell2",
+    "Maxwell3",
+    "Maxwell4",
+    "Maxwell6",
+    "Maxwell7",
+    "Warzone_1",
+    "Warzone_2",
+    "Warzone_3" 
+  },
+  required_prefabs={ "multiplayer_portal" },
+  required_setpieces={ "Sculptures_1", "Maxwell5" },
+  substitutes={  },
+  version=4 
+}
